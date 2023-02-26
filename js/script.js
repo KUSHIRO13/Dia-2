4a9346ccf8617fa86cbe8417263a9a7286c89c91
KUSHIRO13 <kushiroamigama@gmail.com>
2023-02-25 21:32:20 -0500

    menu icons

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const TOGGLE = document.querySelector(".conjunto");
+const LINEAS = document.querySelector(".active");
+
+TOGGLE.addEventListener("click", () => {
+  TOGGLE.classList.toggle("rotar");
+});
